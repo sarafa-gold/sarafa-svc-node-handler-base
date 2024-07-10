4a12386337667c9c9f66c0c1a51c8f58cc7c807a
paragmore <paragmore306@gmail.com>
2024-07-10 19:35:54 +0530

    Add and delete services

--- src/base.ts
+++ src/base.ts
@@ -1,78 +1,120 @@
-import EventEmitter from "events";
+import EventEmitter from 'events'
 import async from 'async'
 
+interface Service {
+  new (args: { opts: Object; name: string }): ServiceInstance
+}
+
+interface ServiceInstance {
+  [key: string]: any
+}
+
+interface Services {
+  [key: string]: ServiceInstance
+}
+
 class Base extends EventEmitter {
-    conf: any;
-    ctx: { root: string; env: "production" | "development"; worker: string; };
-    stopping: boolean = false
-    active: number;
+  conf: any
+  ctx: { root: string; env: 'production' | 'development'; worker: string }
+  stopping: boolean = false
+  active: number
+  servicesArgs: Array<{ opts: Object; name: string; library: string }> = []
+  services: Services = {}
 
-
-    constructor(conf: any, ctx: {
-        root: string;
-        env: "production" | "development";
-        worker: string;
-    }) {
-        super()
-
-        this.conf = conf
-        this.ctx = ctx
-        this.active = 0
+  constructor (
+    conf: any,
+    ctx: {
+      root: string
+      env: 'production' | 'development'
+      worker: string
     }
-    init() {
+  ) {
+    super()
+
+    this.conf = conf
+    this.ctx = ctx
+    this.active = 0
+  }
+  init () {}
+
+  addServices () {
+    this.servicesArgs.forEach(service =>
+      this.addService(service.name, service.library, service.opts)
+    )
+  }
+
+  addService (name: string, library: string, opts: Object) {
+    let svcImport = null
+    try {
+      svcImport = require(library)
+    } catch (e) {
+      console.log('Error occurrec in addService', e, name, library)
     }
-
-    addServices() {
-
+    if (!svcImport) {
+      return null
     }
-
-    deleteServices() {
-
+    const service = new svcImport({ opts, name })
+    if (!this.services[name]) {
+      service.start(() => console.log('Start service', name, library))
+      this.services[name] = service
+    } else {
+      console.log('Service already added', name, library)
     }
+  }
 
-    _start(cb: (err?: Error | null, results?: any) => void) {
-    }
+  deleteServices () {
+    Object.keys(this.services).forEach(serviceName => {
+      this.services[serviceName].stop()
+      delete this.services[serviceName]
+    })
+  }
 
-    _stop(cb: (err?: Error | null, results?: any) => void) {
+  _start (cb: (err?: Error | null, results?: any) => void) {}
 
-    }
+  _stop (cb: (err?: Error | null, results?: any) => void) {}
 
-    stop(cb: (err?: Error | null, results?: any) => void) {
-        const asyncSeries = []
-        asyncSeries.push((next: () => any) => {
-            this._stop(next)
-        })
-        asyncSeries.push((next: () => any) => { this.deleteServices(); next() })
-        asyncSeries.push((next: () => void) => {
-            this.active = 0;
-            next();
-        });
-        this.stopping = true
+  stop (cb: (err?: Error | null, results?: any) => void) {
+    const asyncSeries = []
+    asyncSeries.push((next: () => any) => {
+      this._stop(next)
+    })
+    asyncSeries.push((next: () => any) => {
+      this.deleteServices()
+      next()
+    })
+    asyncSeries.push((next: () => void) => {
+      this.active = 0
+      next()
+    })
+    this.stopping = true
 
-        async.series(asyncSeries, cb)
-    }
-    start(cb: (err?: Error | null, results?: any) => void) {
-        const asyncSeries = []
-        asyncSeries.push((next: () => any) => { this.addServices(); next() })
-        asyncSeries.push((next: () => void) => {
-            this.active = 1;
-            next();
-        });
-        asyncSeries.push((next: () => any) => {
-            this._start(next)
-        })
-        async.series(asyncSeries, (err) => {
-            if (err) {
-                console.trace()
-                throw err
-            }
+    async.series(asyncSeries, cb)
+  }
+  start (cb: (err?: Error | null, results?: any) => void) {
+    const asyncSeries = []
+    asyncSeries.push((next: () => any) => {
+      this.addServices()
+      next()
+    })
+    asyncSeries.push((next: () => void) => {
+      this.active = 1
+      next()
+    })
+    asyncSeries.push((next: () => any) => {
+      this._start(next)
+    })
+    async.series(asyncSeries, err => {
+      if (err) {
+        console.trace()
+        throw err
+      }
 
-            process.nextTick(() => {
-                this.emit('started')
-                cb()
-            })
-        })
-    }
+      process.nextTick(() => {
+        this.emit('started')
+        cb()
+      })
+    })
+  }
 }
 
 export default Base
